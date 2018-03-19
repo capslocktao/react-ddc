3e63a9c70dd046d6b017aa680b722ee6a7f00180
capslocktao <haitaocapslock@outlook.com>
2018-03-19 20:55:58 +0800

    可选择商品列表公共组件完成

--- src/containers/roleCustomer/purchase/purchase.js
+++ src/containers/roleCustomer/purchase/purchase.js
@@ -11,42 +11,78 @@ class Purchase extends Component {
                     productName:"纸尿裤",
                     modelSize:"NB",
                     price:"200",
-                    id:1
+                    id:1,
+                    unit:"包"
                 },
                 {
                     productName:"纸尿裤",
                     modelSize:"S",
                     price:"200",
-                    id:2
+                    id:2,
+                    unit:"包"
                 },
                 {
                     productName:"纸尿裤大号",
                     modelSize:"XXL",
                     price:"210",
-                    id:3
+                    id:3,
+                    unit:"包"
                 },
                 {
                     productName:"纸尿裤",
                     modelSize:"XL",
                     price:"150",
-                    id:4
+                    id:4,
+                    unit:"包"
                 },
                 {
                     productName:"纸尿裤",
                     modelSize:"XL",
                     price:"150",
-                    id:6
+                    id:6,
+                    unit:"包"
                 },
                 {
                     productName:"纸尿裤",
                     modelSize:"L",
                     price:"200",
-                    id:5
+                    id:5,
+                    unit:"包"
                 }
-            ]
+            ],
+            selectedGoods:[],
+            nums:[]
         };
+        this.selectGood = this.selectGood.bind(this)
+        this.numChange = this.numChange.bind(this)
+        this.submitOrder = this.submitOrder.bind(this)
     };
+    selectGood(data){
+        this.setState({
+            selectedGoods:data
+        })
+    };
+    numChange(data){
+        this.setState({
+            nums:data
+        })
+    }
+    submitOrder(){
+        console.log(this.state.selectedGoods);
+        this.state.nums.forEach(v=>{
+            v = parseInt(v)
+        });
+        this.state.goods.findIndex((v,i)=>{
+            this.state.selectedGoods.forEach(k=>{
+                if(k.id === v.id){
+                    console.log(i);
+                    k.num = this.state.nums[i]
+                }
+            })
+        });
 
+        console.log(this.state.selectedGoods)
+    }
     render() {
         return (
             <div className="purchase">
@@ -54,13 +90,17 @@ class Purchase extends Component {
                     <NavBar
                         mode="dark"
                         rightContent={
-                            <Icon type="check" />
+                            this.state.selectedGoods.length === 0?
+                                "":
+                            <div onClick={this.submitOrder}>
+                                下单({this.state.selectedGoods.length})
+                            </div>
                         }
                     >商品采购</NavBar>
                 </div>
 
                 <div className="pruchase-body">
-                    <GoodsList data={this.state.goods}/>
+                    <GoodsList data={this.state.goods} onSelect={this.selectGood} onNumChange={this.numChange}/>
                 </div>
             </div>
         )
