57cf0835d144a00e54fa0ccf5bd179d84a51d766
fzshigs <1440914641@qq.com>
2018-03-28 21:58:22 +0800

    统计

--- src/containers/roleSale/dataStatistics/dataStatistics.js
+++ src/containers/roleSale/dataStatistics/dataStatistics.js
@@ -1,197 +1,206 @@
 import React, {Component} from 'react';
-import "./dataStatistics.less";
-import { NavBar,Icon ,DatePicker, List , Tabs, WhiteSpace, Badge,Flex} from "antd-mobile";
+import {NavBar, Icon, DatePicker, List, Tabs, WhiteSpace, Badge, Flex} from "antd-mobile";
 import axios from 'axios';
-import { API,HOST } from '../../../const/host';
+import {API, HOST} from '../../../const/host';
+import "./dataStatistics.less";
 
-
-const nowTimeStamp = Date.now();
-const now = new Date(nowTimeStamp);
+const nowd = new Date();
+const now = nowd.toLocaleDateString();
+const nowd1 = new Date(new Date().getTime() - 30 * 24 * 3600 * 1000);
+const now1=nowd1.toLocaleDateString();
 const Item = List.Item;
 
-const user=sessionStorage.getItem('user')
+const user = sessionStorage.getItem('user')
 
 
-
-
-
+/*
+* */
 const tabs2 = [
-    { title: '商品',type:"productType" ,sub: '1' },
-    { title: '门店',type:"customerType" ,sub: '2' },
-    { title: '所有',type:"customerType" ,sub: '3' },
+    {title: '商品', type: "goods", sub: '1'},
+    {title: '门店', type: "customer", sub: '2'},
+    {title: '时间', type: "time", sub: '3'},
 ];
 
 
-class TaBle1 extends Component{
-    constructor(props) {
-        super(props);
-        this.state = {
-            data:this.props.data
-        };
-    };
-    render(){
-        return(
-            <table>
-                <thead>
-                <tr>
-                    <th>门店</th>
-                    <th>商品</th>
-                    <th>数量</th>
-                    <th>金额</th>
-                    <th>回款</th>
-                    <th>未回款</th>
-                    {
-                        /*switch (user.roleCode) {
-                            case "sales":
-                               break;
-                        }*/
-                        /*<th>销售员</th>
-                                 <th>分公司</th>*/
-                    }
-
-
-                </tr>
-                </thead>
-                <tbody>
-                <tr>
-                    <td>门店</td>
-                    <td>商品</td>
-                    <td>数量</td>
-                    <td>金额</td>
-                    <td>回款</td>
-                    <td>未回款</td>
-                    {
-                        /*switch (user.roleCode) {
-                            case "sales":
-                               break;
-                        }*/
-                        /*<th>销售员</th>
-                                 <th>分公司</th>*/
-                    }
-                </tr>
-                </tbody>
-            </table>
-        )
-    }
-}
-
-class shpin extends Component{
+class Client extends Component {
     constructor(props){
         super(props);
-        this.state={
-            data:this.props.data
-        }
+        this.state = {
+            list: ""
+        };
+    };
+
+    componentWillReceiveProps(nextProps) {
+        this.setState({
+            list: nextProps.data
+        })
+        console.log(nextProps.data);
+        let map = nextProps.data;
+
     }
-    render(){
-        return(
-            <table>
-                <thead>
-                <tr>
-                    <th>商品</th>
-                    <th>数量</th>
-                    <th>金额</th>
-                    <th>回款</th>
-                    <th>未回款</th>
-                </tr>
-                </thead>
-                <tbody>
-                <tr>
-                    <td>商品</td>
-                    <td>数量</td>
-                    <td>金额</td>
-                    <td>回款</td>
-                    <td>未回款</td>
-                </tr>
-                </tbody>
-            </table>
+
+    render() {
+        return (
+            <div>
+                <div className="count">
+                {
+                    Object.keys(this.state.list).map((key) => (
+                        <div className="count_pack">
+                            <div className="count_left">
+                                {key}
+                            </div>
+                            <div className="count_right">
+                                <div className="count_list_pack">
+                                    <ul className="count_list">
+                                        <li key="0"><span>商品</span><span>数量</span><span>金额</span><span>回款</span><span>未回款</span></li>
+
+                                       {
+                                            this.state.list[key].map((item,index) => (
+                                            <li key={index}>
+                                            <span>{item.goodsName}</span><span>{item.num}</span><span>{item.total}</span><span>{item.receivedPayments}</span><span>{item.unreceived}</span>
+                                            </li>
+
+                                            ))
+                                        }
+                                    </ul>
+                                </div>
+                           </div>
+                      </div>
+                    ))
+                }
+            </div>
+            </div>
         )
     }
 }
 
-/*
-class TaBle1 extends Component{
+
+class Ware extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            data:this.props.data
-        };
-    };
-    render(){
-        return(
-            <table>
-                <thead>
-                <tr>
-                    <th>商品</th>
-                    <th>数量</th>
-                    <th>金额</th>
-                    <th>回款</th>
-                    <th>未回款</th>
-                </tr>
-                </thead>
-                <tbody>
-                <tr>
-                    <td>商品</td>
-                    <td>数量</td>
-                    <td>金额</td>
-                    <td>回款</td>
-                    <td>未回款</td>
-                </tr>
-                </tbody>
-            </table>
+            list: ""
+        }
+
+    }
+
+    componentWillReceiveProps(nextProps) {
+        this.setState({
+            list: nextProps.data
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                {
+                    this.state.list ?
+                        <ul className="goods_list">
+                            <li><span>商品</span><span>数量</span><span>金额</span><span>回款</span><span>未回款</span></li>
+                            {
+                                this.state.list.map((item,index) =>(
+                                    <li key={index}>
+                                        <span>{item.goodsName}</span><span>{item.num}</span><span>{item.total}</span><span>{item.receivedPayments}</span><span>{item.unreceived}</span>
+                                    </li>
+                                ))
+                            }
+                        </ul>
+                        : ""
+                }
+            </div>
         )
     }
 }
-*/
 
-const api="http://192.168.31.34:8080";
+const api = "http://192.168.31.174:8080";
+
 
 class DataStatistics extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            searchTime:now,
-            endTime:now,
-            status:"",
-            list:""
+            searchTime:"",
+            endTime:"",
+            searchTimed:nowd1,
+            endTimed:nowd,
+            status: "goods",
+            list: ""
         };
-        this.TabClick=this.TabClick.bind(this)
-        this.searchClick=this.searchClick.bind(this)
-        this.endClick=this.endClick.bind(this)
+        this.TabClick = this.TabClick.bind(this)
+        this.searchClick = this.searchClick.bind(this)
+        this.endClick = this.endClick.bind(this)
     };
-    componentDidMount(){
-        let params={
-            customerType:"",
-            productType:"productType",
-            searchTime:"2018-3",
-            endTime:"2018-4"
+
+    componentDidMount() {
+        let params = {
+            startTime:now1,
+            endTime:now,
+            countType: this.state.status
         }
-        /*
-        * let params={
-            status:this.state.status,
-            searchTime:this.state.searchTime,
-            endTime:this.state.endTime
-        }*/
-       axios.post(`${api}/base/salesData/findAllSalesData`,params).then(response=>{
-           console.log(response)
-       })
+        /*time,customer,goods*/
+        console.log(params)
+        axios.post(`${api}/base/salesData/count`, params).then(response => {
+            console.log(response)
+            let res = response.data;
+            if (res.result) {
+                this.setState({list: res.data});
+            }
+        })
     }
-    componentWillUnmount(){
+
+    componentWillUnmount() {
 
     }
-    TabClick(tab,index){
-        this.setState({status:tab.type})
+
+    TabClick(tab, index) {
+        this.setState({status: tab.type})
+        let params = {
+            startTime:this.state.searchTime,
+            endTime:this.state.endTime,
+            countType: tab.type
+        }
+        axios.post(`${api}/base/salesData/count`, params).then(response => {
+            // console.log(response)
+            let res = response.data;
+            if (res.result) {
+                this.setState({list: res.data});
+            }
+        })
     }
-    searchClick(date){
-        let sdate=new Date(date);
-        let dateTime = sdate.getFullYear()+'-'+(Number(sdate.getMonth())+1);
-        console.log(dateTime)
-        this.setState({searchTime:dateTime})
+
+    searchClick(date) {
+        console.log(date)
+        let sdate = new Date(date).toLocaleDateString();
+        this.setState({searchTime: sdate,searchTime:date});
+        let params = {
+            startTime:sdate,
+            endTime:this.state.endTime,
+            countType: this.state.status
+        }
+        axios.post(`${api}/base/salesData/count`, params).then(response => {
+            // console.log(response)
+            let res = response.data;
+            if (res.result) {
+                this.setState({list: res.data});
+            }
+        })
     }
-    endClick(date){
-        this.setState({endTime:date})
-        let dat =  this.state.date;
-        let Date = date.getFullYear()+'-'+(Number(date.getMonth())+1);
-        console.log(Date)
+
+    endClick(date) {
+        let sdate = new Date(date).toLocaleDateString();
+        this.setState({endTime: sdate,endTimed:date});
+        let params = {
+            startTime:this.state.searchTime,
+            endTime:sdate,
+            countType: this.state.status
+        }
+        axios.post(`${api}/base/salesData/count`, params).then(response => {
+            // console.log(response)
+            let res = response.data;
+            if (res.result) {
+                this.setState({list: res.data});
+            }
+        })
     }
 
     render() {
@@ -201,31 +210,31 @@ class DataStatistics extends Component {
                     <NavBar
                         mode="dark"
                         icon={<Icon type="left"/>}
-                        onLeftClick={()=>{
+                        onLeftClick={() => {
                             this.props.history.push(`${HOST}/index/salesUserCenter`)
                         }}
                     >
                         数据统计
                     </NavBar>
                 </div>
-                <List className="date-picker-list" style={{ backgroundColor: 'white' }}>
+                <List className="date-picker-list" style={{backgroundColor: 'white'}}>
                     <DatePicker
-                        mode="month"
+                        mode="date"
                         title="Select Date"
                         extra="Optional"
-                        value={this.state.searchTime}
-                        onOk={this.searchClick}
+                        value={this.state.searchTimed}
+                        onChange={this.searchClick}
                     >
-                        <List.Item arrow="horizontal">起始月初</List.Item>
+                        <List.Item arrow="horizontal">起始时间</List.Item>
                     </DatePicker>
                     <DatePicker
-                        mode="month"
+                        mode="date"
                         title="Select Date"
                         extra="Optional"
-                        value={this.state.endTime}
-                        onOk={this.endClick}
+                        value={this.state.endTimed}
+                        onChange={this.endClick}
                     >
-                        <List.Item arrow="horizontal">结束月初</List.Item>
+                        <List.Item arrow="horizontal">结束时间</List.Item>
                     </DatePicker>
                 </List>
                 <div>
@@ -235,26 +244,9 @@ class DataStatistics extends Component {
                     </Tabs>
                 </div>
                 <div className="tabList">
-                    <table>
-                        <thead>
-                        <tr>
-                            <th>商品</th>
-                            <th>数量</th>
-                            <th>金额</th>
-                            <th>回款</th>
-                            <th>未回款</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        <tr>
-                            <td>商品</td>
-                            <td>数量</td>
-                            <td>金额</td>
-                            <td>回款</td>
-                            <td>未回款</td>
-                        </tr>
-                        </tbody>
-                    </table>
+                    {
+                        this.state.status == "goods" ? <Ware data={this.state.list}/> : <Client data={this.state.list}/>
+                    }
                 </div>
             </div>
         )
